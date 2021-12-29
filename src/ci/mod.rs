9986251d0665c74d10636ba005d1f1ecda9d8014
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2021-12-29 12:25:27 +0100

    Always output stdout/stderr

--- src/ci/mod.rs
+++ src/ci/mod.rs
@@ -48,13 +48,11 @@ impl JobRunner for CommandJobRunner {
     fn run(&self, job: String) -> JobOutput {
         match Command::new(&job).output() {
             Ok(output) => {
-                if !output.status.success() {
-                    return match std::str::from_utf8(&output.stdout) {
-                        Ok(e) => JobOutput::JobError(e.to_string()),
-                        Err(e) => JobOutput::ProcessError(format!("{}: {}", job, e)),
-                    };
-                }
-                JobOutput::Success(job)
+                return match (output.status.success(), std::str::from_utf8(&output.stdout)) {
+                    (true, Ok(output)) => JobOutput::Success(output.to_string()),
+                    (false, Ok(e)) => JobOutput::JobError(e.to_string()),
+                    (_, Err(e)) => JobOutput::ProcessError(e.to_string()),
+                };
             }
             Err(e) => JobOutput::ProcessError(format!("{}: {}", job, e)),
         }
