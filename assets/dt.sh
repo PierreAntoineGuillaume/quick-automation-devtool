0f9efc2a7412b72bece19f20e1d22e42d2f17f5c
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2022-01-04 23:52:05 +0100

    add unit tests for autocompletion

--- assets/dt.sh
+++
@@ -1,9 +0,0 @@
-_dt() {
-  local cur
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=($(compgen -W "ci autocomplete" -- "${cur}"))
-  return 0
-}
-
-complete -F _dt dt
---
+++ tools/lib/bash-completion-unit-framework.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# This is a lib for testing bash autocompletion
+# It works by mimicking behavior of what complete should do when tab is pressed
+# given I could not make compgen work
+
+assert() {
+  if [ "${3+set}" = "set" ]; then
+    assertionMessage=$3
+  else
+    assertionMessage="%s != %s\n"
+  fi
+
+  successMessage=${4:-}
+
+  if test "$1" = "$2"; then
+    # shellcheck disable=SC2059
+    printf "$successMessage" "$1"
+  else
+    code=$?
+    # shellcheck disable=SC2059
+    printf >&2 "$assertionMessage" "$1" "$2"
+    return "$code"
+  fi
+}
+
+parse_string_for_completion() {
+  COMP_LINE=$1
+  read -r -a COMP_WORDS <<<"$COMP_LINE"
+
+  # COMP_CWORD must be -1 if the last character is not in the IFS
+  # we're using a sentinel value "word" to concatenate with string
+  # so that if the string ends with a space, it will add a full word to the string
+  # and in all cases, remove one word from the total
+  # https://en.wikipedia.org/wiki/Sentinel_value
+  read -r -a count_word_helper <<<"${COMP_LINE}word"
+  COMP_CWORD="${#count_word_helper[@]}"
+  ((COMP_CWORD -= 1))
+}
+
+autocomplete_test() {
+  exec=$1
+  line=$2
+  parse_string_for_completion "$line"
+  shift 2
+  expected=$*
+  local oldstate
+  oldstate=$(set +o | grep nounset | grep -o -- '[-+]o')
+  set +o nounset
+  $exec
+  set "$oldstate" nounset
+  assert "$expected" "${COMPREPLY[*]}" "$(printf '❌ %-10s expected: "%%s" actual: "%%s"' "$line")\n" "$(printf '✅ %-10s expected: "%%s"' "$line")\n"
+}
+
+(
+  set -euo pipefail
+  auto_test_framework() {
+    # this tests assert
+    if ! assert "5" "5" "error while expecting success: test failed on 5=5\n"; then
+      return 1
+    fi
+    if assert "1" "2" "" "error while expecting error: test did not fail on 1=2\n"; then
+      return 1
+    fi
+
+    # this tests COMP_CWORD positionning
+    set -- "dt " 1 \
+      "dt a" 1 \
+      "dt a " 2
+
+    while [ "$#" -gt 0 ]; do
+      parse_string_for_completion "$1"
+      if ! assert "$2" "$COMP_CWORD" "COMP_CWORD \"$1\" expected: %s actual: %s\n"; then
+        return 1
+      fi
+      shift 2
+    done
+  }
+
+  if ! auto_test_framework; then
+    echo >&2 "$0:" "Framework is bugged, exiting."
+    exit 255
+  fi
+)
