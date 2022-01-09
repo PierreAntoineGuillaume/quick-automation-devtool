a3a29b13619cbcd6b9e1cc8e5393d4122984bb1f
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2022-01-09 12:42:58 +0100

    ref: split start and schedule

--- src/ci/schedule/mod.rs
+++ src/ci/schedule/mod.rs
@@ -4,15 +4,39 @@ use super::CommandJobRunner;
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 
-#[derive(Clone, Copy)]
-pub struct ParrallelJobScheduler {}
+pub trait JobStarter {
+    fn start_all_jobs(&self, jobs: &[Job], first_tx: Sender<JobProgress>);
+}
 
-impl JobScheduler for ParrallelJobScheduler {
+#[derive(Clone, Copy)]
+pub struct ParrallelJobStarter {}
+
+impl JobStarter for ParrallelJobStarter {
+    fn start_all_jobs(&self, jobs: &[Job], first_tx: Sender<JobProgress>) {
+        for real_job in jobs {
+            let job = real_job.clone();
+            let tx = first_tx.clone();
+            thread::spawn(move || {
+                tx.send(JobProgress::new(job.name.clone(), Progress::Started))
+                    .unwrap();
+                let terminated = Progress::Terminated(job.start(&CommandJobRunner::new()));
+                tx.send(JobProgress::new(job.name, terminated)).unwrap();
+            });
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct CompositeJobScheduler<'a> {
+    job_starter: &'a dyn JobStarter,
+}
+
+impl JobScheduler for CompositeJobScheduler<'_> {
     fn schedule(&mut self, jobs: &[Job]) -> Result<(), ()> {
         let (first_tx, rx) = channel();
 
         Self::signal_all_existing_jobs(jobs, &first_tx);
-        Self::start_all_jobs(jobs, first_tx);
+        self.job_starter.start_all_jobs(jobs, first_tx);
 
         let mut pipeline_progress = PipelineProgress::new();
         let mut is_error = false;
@@ -35,7 +59,7 @@ impl JobScheduler for ParrallelJobScheduler {
     }
 }
 
-impl ParrallelJobScheduler {
+impl CompositeJobScheduler<'_> {
     fn signal_all_existing_jobs(jobs: &[Job], first_tx: &Sender<JobProgress>) {
         for job in jobs {
             first_tx
@@ -43,17 +67,7 @@ impl ParrallelJobScheduler {
                 .unwrap();
         }
     }
-
-    fn start_all_jobs(jobs: &[Job], first_tx: Sender<JobProgress>) {
-        for real_job in jobs {
-            let job = real_job.clone();
-            let tx = first_tx.clone();
-            thread::spawn(move || {
-                tx.send(JobProgress::new(job.name.clone(), Progress::Started))
-                    .unwrap();
-                let terminated = Progress::Terminated(job.start(&CommandJobRunner::new()));
-                tx.send(JobProgress::new(job.name, terminated)).unwrap();
-            });
-        }
+    pub fn new(job_starter: &mut dyn JobStarter) -> CompositeJobScheduler {
+        CompositeJobScheduler { job_starter }
     }
 }
