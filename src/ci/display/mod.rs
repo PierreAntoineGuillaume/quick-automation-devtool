9c64ebfd539252aa568ec4144bd96d0770ae7a54
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2022-02-01 20:31:45 +0100

    feat(ci): clean up output

--- src/ci/display/mod.rs
+++ src/ci/display/mod.rs
@@ -7,7 +7,7 @@ pub struct OneOffCiDisplay {}
 impl CiDisplay for OneOffCiDisplay {
     fn refresh(&mut self, tracker: &JobProgressTracker) {
         if tracker.is_finished() {
-            println!("{}", tracker)
+            print!("{}", tracker)
         }
     }
 }
@@ -19,8 +19,17 @@ impl OneOffCiDisplay {
 }
 
 const CHECK: &str = "✔";
-const CROSS: &str = "❌";
-const SKULL: &str = "💀";
+const CROSS: &str = "✕";
+const SKULL: &str = "�";
+
+fn try_cleanup(input: String) -> String {
+    let cleaned = input.trim_end();
+    if cleaned.is_empty() {
+        String::new()
+    } else {
+        format!("{cleaned}\n")
+    }
+}
 
 impl std::fmt::Display for JobProgressTracker {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -29,19 +38,27 @@ impl std::fmt::Display for JobProgressTracker {
             for progress in &progress_collector.progresses {
                 match progress {
                     Progress::Partial(instruction, job_output) => match job_output {
-                        JobOutput::Success(stdout, stderr) => {
-                            write!(f, "{CHECK} {instruction}\n{stdout}\n{stderr}")?;
-                        }
-                        JobOutput::JobError(stdout, stderr) => {
-                            write!(f, "{CROSS} {instruction}\n{stdout}\n{stderr}")?;
+                        JobOutput::Success(stdout, stderr)
+                        | JobOutput::JobError(stdout, stderr) => {
+                            let symbol = if job_output.succeeded() { CHECK } else { CROSS };
+                            write!(
+                                f,
+                                "{symbol} {}",
+                                try_cleanup(format!(
+                                    "{}{}{}",
+                                    instruction,
+                                    try_cleanup(stdout.clone()),
+                                    try_cleanup(stderr.clone())
+                                ))
+                            )?;
                         }
                         JobOutput::ProcessError(stderr) => {
-                            write!(f, "{SKULL} {instruction}: {stderr}")?;
+                            write!(f, "{SKULL} {instruction}: {}", try_cleanup(stderr.clone()))?;
                         }
                     },
                     Progress::Terminated(bool) => {
                         let emoji: &str = if *bool { CHECK } else { CROSS };
-                        writeln!(f, "{emoji} job {job_name}")?;
+                        writeln!(f, "{emoji} all tasks done for job {job_name}")?;
                     }
                     _ => {}
                 }
