ba6441f71968fa90590163c126d2df8bb5c1cd33
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2022-02-01 19:48:42 +0100

    feat(ci): improve job output

--- src/ci/display/mod.rs
+++ src/ci/display/mod.rs
@@ -18,25 +18,30 @@ impl OneOffCiDisplay {
     }
 }
 
+const CHECK: &str = "✔";
+const CROSS: &str = "❌";
+const SKULL: &str = "💀";
+
 impl std::fmt::Display for JobProgressTracker {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         for (job_name, progress_collector) in &self.states {
-            writeln!(f, "job {}: ...", job_name)?;
+            writeln!(f, "Running tasks for job {job_name}")?;
             for progress in &progress_collector.progresses {
                 match progress {
                     Progress::Partial(instruction, job_output) => match job_output {
                         JobOutput::Success(stdout, stderr) => {
-                            write!(f, "✅ {}\n{}\n{}", instruction, stdout, stderr)?;
+                            write!(f, "{CHECK} {instruction}\n{stdout}\n{stderr}")?;
                         }
                         JobOutput::JobError(stdout, stderr) => {
-                            write!(f, "❌ {}\n{}\n{}", instruction, stdout, stderr)?;
+                            write!(f, "{CROSS} {instruction}\n{stdout}\n{stderr}")?;
                         }
                         JobOutput::ProcessError(stderr) => {
-                            write!(f, "💀 {}: {}", instruction, stderr)?;
+                            write!(f, "{SKULL} {instruction}: {stderr}")?;
                         }
                     },
                     Progress::Terminated(bool) => {
-                        writeln!(f, "{} job {}", if *bool { "✅" } else { "❌" }, job_name)?;
+                        let emoji: &str = if *bool { CHECK } else { CROSS };
+                        writeln!(f, "{emoji} job {job_name}")?;
                     }
                     _ => {}
                 }
